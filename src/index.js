cdcbc73b4993fe47063f283813085e7b7db88a65
Antonis <marasantonis@gmail.com>
2022-05-11 21:39:46 +0300

    css path added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import "./css/shared-style.css";
-import "./css/home-section-style.css";
-import "./css/project-section-style.css";
-import "./css/main-section-style.css";
-import "./css/task-properties-style.css";
+import "./css/main-middle-style.css";
+import "./css/main-right-style.css";
+import "./css/main-left-style.css";
 import html from "./admin.html";
