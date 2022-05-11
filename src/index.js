0afa30cf6b238488e9831e9e9b5aae91a9422a5f
Antonis <marasantonis@gmail.com>
2022-05-11 11:20:14 +0300

    path and markup update

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import "./css/shared-style.css";
 import "./css/home-section-style.css";
 import "./css/project-section-style.css";
+import "./css/main-section-style.css";
 import html from "./admin.html";
