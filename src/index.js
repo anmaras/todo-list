ea2ee31825b66ba2654dc5e8131d0d5af99d490e
Antonis <marasantonis@gmail.com>
2022-05-12 13:36:51 +0300

    css paths added

--- src/index.js
+++ src/index.js
@@ -2,4 +2,5 @@ import "./css/shared-style.css";
 import "./css/main-middle-style.css";
 import "./css/main-right-style.css";
 import "./css/main-left-style.css";
+import "./css/header-style.css";
 import html from "./admin.html";
