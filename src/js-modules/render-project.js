4a4c63e58e6b2b7b4fba5e375a2ea10b57d856e5
Antonis <marasantonis@gmail.com>
2022-05-17 20:24:15 +0300

    add variables for svgs and so to reduce the size of markup

--- src/js-modules/render-project.js
+++ src/js-modules/render-project.js
@@ -1,9 +1,24 @@
 import { projectListDomElement } from "./dom-elements";
 import { createNewProjectInstance } from "./create-project";
 
-function renderProjectListItem() {
-  const markup = `<li>
-                  <svg
+/* Garbage Icon */
+const garbageIcon = `<svg
+  id="garbageIcon"
+  xmlns="http://www.w3.org/2000/svg"
+  xmlns:xlink="http://www.w3.org/1999/xlink"
+  version="1.1"
+  width="24"
+  height="24"
+  viewBox="0 0 24 24"
+>
+  <path
+    d="M9,3V4H4V6H5V19A2,2 0 0,0 7,21H17A2,2 0 0,0 19,19V6H20V4H15V3H9M7,6H17V19H7V6M9,8V17H11V8H9M13,8V17H15V8H13Z"
+  />
+</svg>
+`;
+
+/* Project Icon */
+const projectIcon = `<svg
                     xmlns="http://www.w3.org/2000/svg"
                     xmlns:xlink="http://www.w3.org/1999/xlink"
                     version="1.1"
@@ -14,24 +29,14 @@ function renderProjectListItem() {
                     <path
                       d="M20,18H4V8H20M20,6H12L10,4H4C2.89,4 2,4.89 2,6V18A2,2 0 0,0 4,20H20A2,2 0 0,0 22,18V8C22,6.89 21.1,6 20,6Z"
                     />
-                  </svg> 
-                  <div class="left-section__projects__user-actions">
-                    <button>Rename</button>
-                    <button>Delete</button>
-                  </div>
+                  </svg> `;
+
+/* Render Dom Elements Function */
+function renderProjectListItem() {
+  const markup = `<li class="project-list__item">
+                  ${projectIcon}
                   <p>${createNewProjectInstance().name}</p>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    xmlns:xlink="http://www.w3.org/1999/xlink"
-                    version="1.1"
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                  >
-                    <path
-                      d="M12,16A2,2 0 0,1 14,18A2,2 0 0,1 12,20A2,2 0 0,1 10,18A2,2 0 0,1 12,16M12,10A2,2 0 0,1 14,12A2,2 0 0,1 12,14A2,2 0 0,1 10,12A2,2 0 0,1 12,10M12,4A2,2 0 0,1 14,6A2,2 0 0,1 12,8A2,2 0 0,1 10,6A2,2 0 0,1 12,4Z"
-                    />
-                  </svg>
+                  ${garbageIcon}
                 </li>`;
   projectListDomElement.insertAdjacentHTML("beforeend", markup);
 
