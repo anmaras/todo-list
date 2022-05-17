3a90f888827cb408c0b8b730a42bfbbeec9f8b44
Antonis <marasantonis@gmail.com>
2022-05-17 10:56:53 +0300

    create a function tha makes new project instances through class Project

--- src/js-modules/create-project.js
+++ src/js-modules/create-project.js
@@ -1,1 +1,9 @@
-function createNewProjectInstance() {}
+import { Project } from "./project-class";
+import { projectInputDomElement } from "./dom-elements";
+
+function createNewProjectInstance() {
+  const newProject = new Project(projectInputDomElement.value);
+  return newProject;
+}
+
+export { createNewProjectInstance };
