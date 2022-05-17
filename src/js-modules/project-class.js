82b5ee1a89017384d28a8658edbceda1bf9836bf
Antonis <marasantonis@gmail.com>
2022-05-17 11:52:52 +0300

    add static method to push to aexternal array class instances

--- src/js-modules/project-class.js
+++ src/js-modules/project-class.js
@@ -1,6 +1,15 @@
+import { projectArray } from "./arrays";
+
 class Project {
   constructor(name) {
     this.name = name;
+    /* add static method to constructor so every time an instance
+    is created it will be stored to the external project array */
+    Project.addToArray(this);
+  }
+
+  static addToArray(instance) {
+    projectArray.push(instance);
   }
 }
 
