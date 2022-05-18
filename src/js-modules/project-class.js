bf12394d07b0cca0288c40b7688fc852b551054e
Antonis <marasantonis@gmail.com>
2022-05-18 20:39:02 +0300

    add increment id property in class constructor

--- src/js-modules/project-class.js
+++ src/js-modules/project-class.js
@@ -1,8 +1,11 @@
 import { projectArray } from "./arrays";
 
 class Project {
+  static id = 0;
+
   constructor(name) {
     this.name = name;
+    this.id = Project.id++;
     /* add static method to constructor so every time an instance
     is created it will be stored to the external project array */
     Project.addToArray(this);
