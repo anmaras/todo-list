3534e3ecd35840b7e31aefac002859ef9c679b1d
Antonis <marasantonis@gmail.com>
2022-05-21 12:24:59 +0300

    refactor the render function to use this as properties

--- src/js-modules/render-project.js
+++ src/js-modules/render-project.js
@@ -115,33 +115,29 @@ const crossIcon = `<svg
 const star = `<svg id="garbageIcon"xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" width="24" height="24" viewBox="0 0 24 24"><path d="M12,17.27L18.18,21L16.54,13.97L22,9.24L14.81,8.62L12,2L9.19,8.62L2,9.24L7.45,13.97L5.82,21L12,17.27Z" /></svg>`;
 
 /* Render Project Dom Elements Function */
-function renderProjectListItem(title, id) {
+function renderProjectListItem() {
   const createProjectElement = document.createElement("li");
-  createProjectElement.id = id;
+  createProjectElement.id = this.id;
   createProjectElement.className = "project-list__item";
 
   const projectListLabelInputMarkUp = `
-                  ${projectIcon}
-                  
-                  <input value="${title}" id="projectInput"  ></>
-                  
-                              <img src=${Icon} id="garbageIcon">
-`;
+                    ${projectIcon}
+                    <input value="${this.name}" id="projectInput"  ></>
+                                <img src=${Icon} id="garbageIcon">`;
   createProjectElement.insertAdjacentHTML(
     "beforeend",
     projectListLabelInputMarkUp
   );
 
   projectListDomElement.append(createProjectElement);
-
   return projectListDomElement;
 }
 
-function renderTodoHeaderTitle(projectTitle, projectId) {
+function renderTodoHeaderTitle() {
   todoHeaderContainer.replaceChildren();
   const createTodoTitle = document.createElement("h2");
-  createTodoTitle.className = `projectId ${projectId}`;
-  createTodoTitle.textContent = projectTitle;
+  createTodoTitle.className = ".main__container__header-title";
+  createTodoTitle.textContent = this.name;
   todoHeaderContainer.append(createTodoTitle);
 
   return todoHeaderContainer;
