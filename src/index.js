99606d746a04119ba71781b233441db3bb2c8344
Antonis <marasantonis@gmail.com>
2022-05-21 16:07:32 +0300

    update content

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import "./css/left-section/main-left-style.css";
 import "./css/left-section/add-project-btn.css";
 import "./css/left-section/home-container.css";
 import "./css/left-section/project-container.css";
-import "./css/left-section/project-create-rename-form.css";
+import "./css/left-section/project-create-form.css";
 import "./css/middle-section/middle-header.css";
 import "./css/middle-section/middle-sorting-box.css";
 import "./css/middle-section/middle-todo-list.css";
