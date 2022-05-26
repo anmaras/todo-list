07b4a56ec438161534a7bb40fb3b63041d52842a
Antonis <marasantonis@gmail.com>
2022-05-26 18:02:06 +0300

    add on select todo data id and to the main todo li class to dynamically change with select

--- src/js-modules/render-project.js
+++ src/js-modules/render-project.js
@@ -51,7 +51,7 @@ function renderProjectListItem() {
 function renderProjectTodoListItem() {
   const todoList = document.querySelector(".main__task-list__list");
 
-  const listItemMarkup = `<li class="main__task-list__list-item" data-todo-id = ${this.todoId} data-projectId = ${this.projectId}>
+  const listItemMarkup = `<li class="main__task-list__list-item ${this.priority}" data-todo-id = ${this.todoId} data-projectId = ${this.projectId}>
 
                 <!-- Header -->
                 <div class="main__task-list__list-item__title-container">
@@ -100,6 +100,7 @@ function renderProjectTodoListItem() {
                   <select
                     name="select-priority"
                     id="main__task-list__list-item__priority"
+                    data-select-id = ${this.todoId}
                   >
                     <option value="none" selected="selected" disabled>
                       Select an Option
