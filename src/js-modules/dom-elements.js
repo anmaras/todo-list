3b143f79f9529c05ed39ebec44c07f1cff9672a7
Antonis <marasantonis@gmail.com>
2022-05-27 20:23:50 +0300

    prettier line break change

--- src/js-modules/dom-elements.js
+++ src/js-modules/dom-elements.js
@@ -1,7 +1,5 @@
 /* Project Input Dom Element */
-const projectInputDomElement = document.querySelector(
-  "#main__task-list__add-task"
-);
+const projectInputDomElement = document.querySelector("#main__task-list__add-task");
 /* Project List Dom Element */
 const projectListDomElement = document.querySelector(
   ".left-section__projects__container ul"
@@ -11,9 +9,7 @@ const projectListItem = document.querySelectorAll(".project-list__item");
 
 const projectInputValue = document.querySelector("#project");
 
-const homeListDomeElement = document.querySelector(
-  ".left-section__home-container"
-);
+const homeListDomeElement = document.querySelector(".left-section__home-container");
 
 const notProjectYetContainer = document.querySelector(
   ".middle-section__noprojects__container"
