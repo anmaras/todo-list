5135b3e8a6e7b7ac464168bf6e355a3f62d71b5f
Antonis <marasantonis@gmail.com>
2022-06-01 13:22:25 +0300

    remove class bethods because of json

--- src/js-modules/project-class.js
+++ src/js-modules/project-class.js
@@ -19,58 +19,6 @@ class Todo {
     this.priority = "";
     this.checkbox = "";
   }
-
-  select() {
-    if (!this.priority) {
-      let text = "Select an option";
-      return text;
-    }
-    if (this.priority) {
-      return this.priority.charAt(0).toUpperCase() + this.priority.slice(1);
-    }
-  }
-
-  changeToNum(input) {
-    let num;
-    if (input === "low") {
-      num = 3;
-      return num;
-    }
-    if (input === "medium") {
-      num = 2;
-      return num;
-    }
-    if (input === "high") {
-      num = 1;
-      return num;
-    }
-    if (!input || input === "no") {
-      num = 4;
-      return num;
-    }
-  }
-
-  test() {
-    console.log("test");
-  }
-
-  classSetForSelect() {
-    if (!this.priority) {
-      return this.priority;
-    }
-    if (this.priority) {
-      return this.priority.toLowerCase();
-    }
-  }
-
-  classSetForCheck() {
-    if (!this.checkbox) {
-      return "";
-    }
-    if (this.checkbox) {
-      return "disabled";
-    }
-  }
 }
 
 export { Project, Todo };
